df1f1604a28359b740a90a34df7acb914e5622e9
DanyaSantos <santosresendezde1720@gmail.com>
2023-04-18 20:44:53 -0600

    create mongodb base connection

---
+++ Mongo-Api-Books/src/index.js
@@ -0,0 +1,6 @@
+import app from "./server.js"
+ async function runserver() {
+    await app.listen(app.get('port'));
+    console.log('server ready', app.get('port'));
+}
+runserver()
---
+++ Mongo-Api-Books/src/server.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+const app = express();
+//setting
+app.set('port',3000);
+//Middlewares
+//Routes
+
+export default app;
